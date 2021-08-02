e5e3f19cf29b9216e78e60227af0c1d54d2fc295
Yodarango <danranngel@gmail.com>
2021-08-02 14:41:18 -0400

    fixed api

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const app = express();
 const podcast = require("./data/podcast");
 
 app.get("/library", (req, res) => {
-  res.json(podcast);
+  res.send(podcast);
 });
+
 app.listen(process.env.PORT || 8081, () => {
   console.log("Running safely on port");
 });
