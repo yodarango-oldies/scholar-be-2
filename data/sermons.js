b875119516055092d9d65aaecafe7e9a83211320
Yodarango <danranngel@gmail.com>
2021-08-04 11:45:46 -0400

    added colors, tas, and id fileds to sermons

--- data/sermons.js
+++ data/sermons.js
@@ -1,6 +1,10 @@
 module.exports = sermons = [
   {
     id: "sfsegs",
+    userId: "2fds",
+    userSignature: "#XTD",
+    tags: ["#BL", "#RD"],
+    colors: ["rgb(1, 96, 240)", "rgb(224, 0, 0)"],
     title: "Broken Pieces",
     author: "John Doe",
     reviews: ["23", "ef", "fww", "wefs"],
@@ -11,6 +15,10 @@ module.exports = sermons = [
   },
   {
     id: "sfsesdgeqgs",
+    userId: "2fdfqs",
+    userSignature: "#XRQ",
+    tags: ["#GRN", "#OR"],
+    colors: ["rgb(21, 158, 2)", "rgb(247, 99, 0)"],
     title: "Your Parenthesis",
     author: "Thomas Doe",
     reviews: ["23", "ef", "fww", "wefs", "23", "ef", "fww", "wefs"],
@@ -21,6 +29,10 @@ module.exports = sermons = [
   },
   {
     id: "sfsweg45egs",
+    userId: "2fd532s",
+    userSignature: "#ZAC",
+    tags: ["#OR", "#RD"],
+    colors: ["rgb(247, 99, 0)", "rgb(224, 0, 0)"],
     title: "The God Who Gets In The Boat With You",
     author: "Allan Doe",
     reviews: ["23", "ef", "fww", "wefs"],
@@ -32,6 +44,8 @@ module.exports = sermons = [
   {
     id: "sf3252gfdsegs",
     title: "When God Has To",
+    tags: ["#RD", "#OR"],
+    colors: ["rgb(224, 0, 0)", "rgb(247, 99, 0)"],
     author: "Peter Doe",
     reviews: ["23", "ef", "fww", "wefs", "sdf"],
     stars: [5, 5, 5, 5, 5],
@@ -42,6 +56,8 @@ module.exports = sermons = [
   {
     id: "s34547fsegs",
     title: "Going Back To Bethel",
+    tags: ["#BLK", "#CY"],
+    colors: ["rgb(32, 32, 32)", "rgb(247, 99, 0)"],
     author: "Tonny Doe",
     reviews: ["23", "fww", "wefs"],
     stars: [1, 5, 2],
