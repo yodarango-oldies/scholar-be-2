ce7f3378e62c71fff428dbfbf498dfc61be06cbc
Yodarango <danranngel@gmail.com>
2021-08-28 11:14:40 -0400

    added website link to congregations

--- data/congregaions.js
+++ data/congregaions.js
@@ -9,6 +9,7 @@ module.exports = congregations = [
     city: "maryville",
     state: "Tennessee",
     zip: "37777",
+    website: "https://factv.org",
     schedule: ["Sunday 6pm", "Sunday 10am", "Wednesday  7:30"],
     location:
       "https://www.google.com/maps/place/1331+William+Blount+Dr,+Maryville,+TN+37801/@35.7395554,-84.0432936,17z/data=!3m1!4b1!4m5!3m4!1s0x885e9db9233bed8b:0x511a11a4d259b82a!8m2!3d35.7395554!4d-84.0411103",
@@ -23,6 +24,7 @@ module.exports = congregations = [
     city: "maryville",
     state: "Tennessee",
     zip: "37777",
+    website: "https://factv.org",
     schedule: ["Sunday 6pm", "Sunday 10am", "Wednesday  7:30"],
     location:
       "https://www.google.com/maps/place/1331+William+Blount+Dr,+Maryville,+TN+37801/@35.7395554,-84.0432936,17z/data=!3m1!4b1!4m5!3m4!1s0x885e9db9233bed8b:0x511a11a4d259b82a!8m2!3d35.7395554!4d-84.0411103",
@@ -37,6 +39,7 @@ module.exports = congregations = [
     city: "maryville",
     state: "Tennessee",
     zip: "37777",
+    website: "https://factv.org",
     schedule: ["Sunday 6pm", "Sunday 10am", "Wednesday  7:30"],
     location:
       "https://www.google.com/maps/place/1331+William+Blount+Dr,+Maryville,+TN+37801/@35.7395554,-84.0432936,17z/data=!3m1!4b1!4m5!3m4!1s0x885e9db9233bed8b:0x511a11a4d259b82a!8m2!3d35.7395554!4d-84.0411103",
@@ -51,6 +54,7 @@ module.exports = congregations = [
     city: "maryville",
     state: "Tennessee",
     zip: "37777",
+    website: "https://factv.org",
     schedule: ["Sunday 6pm", "Sunday 10am", "Wednesday  7:30"],
     location:
       "https://www.google.com/maps/place/1331+William+Blount+Dr,+Maryville,+TN+37801/@35.7395554,-84.0432936,17z/data=!3m1!4b1!4m5!3m4!1s0x885e9db9233bed8b:0x511a11a4d259b82a!8m2!3d35.7395554!4d-84.0411103",
@@ -65,6 +69,7 @@ module.exports = congregations = [
     city: "maryville",
     state: "Tennessee",
     zip: "37777",
+    website: "https://factv.org",
     schedule: ["Sunday 6pm", "Sunday 10am", "Wednesday  7:30"],
     location:
       "https://www.google.com/maps/place/1331+William+Blount+Dr,+Maryville,+TN+37801/@35.7395554,-84.0432936,17z/data=!3m1!4b1!4m5!3m4!1s0x885e9db9233bed8b:0x511a11a4d259b82a!8m2!3d35.7395554!4d-84.0411103",
