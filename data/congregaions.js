92cd984d9289d9bcaf4a946d2855adc5fad4d233
Yodarango <danranngel@gmail.com>
2021-08-28 12:24:20 -0400

    added iframe to church

--- data/congregaions.js
+++ data/congregaions.js
@@ -13,6 +13,8 @@ module.exports = congregations = [
     schedule: ["Sunday 6pm", "Sunday 10am", "Wednesday  7:30"],
     location:
       "https://www.google.com/maps/place/1331+William+Blount+Dr,+Maryville,+TN+37801/@35.7395554,-84.0432936,17z/data=!3m1!4b1!4m5!3m4!1s0x885e9db9233bed8b:0x511a11a4d259b82a!8m2!3d35.7395554!4d-84.0411103",
+    iFrame:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.457621867182!2d-84.04329898473962!3d35.73955538018072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885e9db9233bed8b%3A0x511a11a4d259b82a!2s1331%20William%20Blount%20Dr%2C%20Maryville%2C%20TN%2037801!5e0!3m2!1sen!2sus!4v1630167556290!5m2!1sen!2sus",
   },
   {
     id: "sfsesdgeqgs",
@@ -28,6 +30,8 @@ module.exports = congregations = [
     schedule: ["Sunday 6pm", "Sunday 10am", "Wednesday  7:30"],
     location:
       "https://www.google.com/maps/place/1331+William+Blount+Dr,+Maryville,+TN+37801/@35.7395554,-84.0432936,17z/data=!3m1!4b1!4m5!3m4!1s0x885e9db9233bed8b:0x511a11a4d259b82a!8m2!3d35.7395554!4d-84.0411103",
+    iFrame:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.457621867182!2d-84.04329898473962!3d35.73955538018072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885e9db9233bed8b%3A0x511a11a4d259b82a!2s1331%20William%20Blount%20Dr%2C%20Maryville%2C%20TN%2037801!5e0!3m2!1sen!2sus!4v1630167556290!5m2!1sen!2sus",
   },
   {
     id: "sfsweg45egs",
@@ -43,6 +47,8 @@ module.exports = congregations = [
     schedule: ["Sunday 6pm", "Sunday 10am", "Wednesday  7:30"],
     location:
       "https://www.google.com/maps/place/1331+William+Blount+Dr,+Maryville,+TN+37801/@35.7395554,-84.0432936,17z/data=!3m1!4b1!4m5!3m4!1s0x885e9db9233bed8b:0x511a11a4d259b82a!8m2!3d35.7395554!4d-84.0411103",
+    iFrame:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.457621867182!2d-84.04329898473962!3d35.73955538018072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885e9db9233bed8b%3A0x511a11a4d259b82a!2s1331%20William%20Blount%20Dr%2C%20Maryville%2C%20TN%2037801!5e0!3m2!1sen!2sus!4v1630167556290!5m2!1sen!2sus",
   },
   {
     id: "sf3252gfdsegs",
@@ -58,6 +64,8 @@ module.exports = congregations = [
     schedule: ["Sunday 6pm", "Sunday 10am", "Wednesday  7:30"],
     location:
       "https://www.google.com/maps/place/1331+William+Blount+Dr,+Maryville,+TN+37801/@35.7395554,-84.0432936,17z/data=!3m1!4b1!4m5!3m4!1s0x885e9db9233bed8b:0x511a11a4d259b82a!8m2!3d35.7395554!4d-84.0411103",
+    iFrame:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.457621867182!2d-84.04329898473962!3d35.73955538018072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885e9db9233bed8b%3A0x511a11a4d259b82a!2s1331%20William%20Blount%20Dr%2C%20Maryville%2C%20TN%2037801!5e0!3m2!1sen!2sus!4v1630167556290!5m2!1sen!2sus",
   },
   {
     id: "s34547fsegs",
@@ -73,5 +81,7 @@ module.exports = congregations = [
     schedule: ["Sunday 6pm", "Sunday 10am", "Wednesday  7:30"],
     location:
       "https://www.google.com/maps/place/1331+William+Blount+Dr,+Maryville,+TN+37801/@35.7395554,-84.0432936,17z/data=!3m1!4b1!4m5!3m4!1s0x885e9db9233bed8b:0x511a11a4d259b82a!8m2!3d35.7395554!4d-84.0411103",
+    iFrame:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.457621867182!2d-84.04329898473962!3d35.73955538018072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885e9db9233bed8b%3A0x511a11a4d259b82a!2s1331%20William%20Blount%20Dr%2C%20Maryville%2C%20TN%2037801!5e0!3m2!1sen!2sus!4v1630167556290!5m2!1sen!2sus",
   },
 ];
